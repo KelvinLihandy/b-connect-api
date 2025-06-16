eb89de469a60776fbeddf66a5062bae196316171
KelvinLihandy <kelvinlihandy2005@gmail.com>
2025-06-16 17:39:57 +0700

    ANOMALI BUDI

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:3000',
   'http://127.0.0.1:3000',
+  'http://localhost:5000',
+  'http://127.0.0.1:5000',
   'https://b-connect-nu.vercel.app',
 ];
 
